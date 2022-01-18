3d31ae7483e6221dca88dc4bff9e1a4b4a7beb60
Kelly Sovacool <kellysovacool@gmail.com>
2022-01-18 15:26:50 -0500

    Fix column selection

--- code/join_metadata.R
+++ code/join_metadata.R
@@ -6,12 +6,12 @@ library(tidyverse)
 metadata <- read_excel(here('data', 'raw', 'ml_metadata.xlsx'))
 otudata <- fread(here('data', 'raw', 'sample.final.shared'))
 
-dat <- inner_join(metadata %>% select(pos_cdiff_d1, group),
-                  otudata %>% rename(group = Group) %>% select(-label, numOtus),
+dat <- inner_join(metadata,
+                  otudata %>% rename(group = Group),
                   by = "group")
 
 fwrite(dat %>% select(pos_cdiff_d1, starts_with('Otu')),
-       file = here('data', 'processed', 'otu_d0.csv'))
+       file = here('data', 'processed', 'otu_day0.csv'))
 
-saveRDS(dat %>% pull(cage),
+saveRDS(dat %>% pull("cage"),
         file = here('data', 'processed', 'cages.Rds'))
