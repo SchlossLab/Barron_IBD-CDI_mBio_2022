88a7f9846e682b53f973486868b20f6ca498fc7b
Kelly Sovacool <kellysovacool@gmail.com>
2022-01-29 01:50:35 -0500

    Improve hp plot titles

--- code/combine_hp_perf.R
+++ code/combine_hp_perf.R
@@ -3,5 +3,5 @@ source("code/log_smk.R")
 models <- lapply(snakemake@input[["rds"]], function(x) readRDS(x))
 hp_perf <- mikropml::combine_hp_performance(models)
 hp_perf$method <- snakemake@wildcards[["method"]]
-hp_perf$group_colname <- snakemake@wildcards[["group_colname"]]
+hp_perf$groups <- snakemake@wildcards[["group_colname"]]
 saveRDS(hp_perf, file = snakemake@output[["rds"]])
