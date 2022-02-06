05b562c68ad963206fc8474bf5b40ebb060053e9
Kelly Sovacool <kellysovacool@gmail.com>
2022-02-05 21:13:05 -0500

    Write code to plot ROC & PRC curves

--- code/plot_roc.R
+++ code/plot_roc.R
@@ -1,80 +1,74 @@
-get_sens_spec_lookup <- function(data){
+library(tidyverse)
+blues <- RColorBrewer::brewer.pal(name='Blues', n = 9)
+greens <- RColorBrewer::brewer.pal(name='Greens', n = 9)
 
-  total <- count(data, observed) %>%
-    pivot_wider(names_from="observed", values_from="n") %>%
-    rename(srn=cancer,healthy=normal)
+dat <- read_csv(snakemake@input[['csv']]) %>%
+    mutate(seed = as.character(seed))
 
-  data %>%
-    arrange(desc(prob_srn)) %>%
-    mutate(is_srn = observed == "cancer") %>%
-    mutate(tp = cumsum(is_srn),
-           fp = cumsum(!is_srn),
-           sensitivity = tp / total$srn,
-           fpr = fp / total$healthy) %>%
-    mutate(specificity = 1- fpr) %>%
-    select(sensitivity, specificity, fpr)
+auroc_step <- dat %>%
+  ggplot(aes(x = fpr,y = sensitivity, color = seed)) +
+  geom_step() +
+  theme_bw() +
+  labs(x="False Positive Rate",
+       y="True Positive Rate") +
+  geom_abline(intercept = 0,lty="dashed",color="grey50") +
+  scale_color_grey() +
+  theme(legend.position = 'none')
 
-}
+# sensitivity vs specificity
+auroc <- dat %>%
+    mutate(specificity = round(specificity, 2)) %>%
+    group_by(specificity) %>%
+    summarise(mean_sensitivity = mean(sensitivity),
+              sd_sensitivity = sd(sensitivity)) %>%
+    mutate(upper_sens = mean_sensitivity + sd_sensitivity,
+           lower_sens = mean_sensitivity - sd_sensitivity) %>%
+    mutate(
+        upper_sens = case_when(upper_sens > 1 ~ 1,
+                               TRUE ~ upper_sens),
+        lower_sens = case_when(upper_sens < 0 ~ 0,
+                               TRUE ~ lower_sens)
+    ) %>%
+    ggplot(aes(x = specificity, y = mean_sensitivity,
+             ymin = lower_sens, ymax = upper_sens)) +
+    geom_ribbon(fill = blues[3]) +
+    geom_line(color = blues[9]) +
+    coord_equal() +
+    geom_abline(intercept = 1, slope = 1, linetype="dashed", color="grey50") +
+    scale_y_continuous(expand = c(0,0), limits = c(-0.01,1.01)) +
+    scale_x_reverse(expand = c(0,0), limits = c(1.01,-0.01)) +
+    labs(x = 'Specificity', y = 'Sensitivity') +
+    guides(color = guide_legend(nrow = 1,title=""),
+           fill = guide_legend(nrow = 1,title="")) +
+    theme_bw() #+theme(plot.margin = unit(x = c(0, 0, 0, 0), units = "pt"))
 
-get_sensitivity <- function(x, lookup){
+# precision vs recall
+auprc <- dat %>%
+    rename(recall = sensitivity) %>%
+    mutate(recall = round(recall, 2)) %>%
+    group_by(recall) %>%
+    summarise(mean_precision = mean(precision),
+              sd_precision = sd(precision)) %>%
+    mutate(upper = mean_precision + sd_precision,
+           lower = mean_precision - sd_precision) %>%
+    mutate(
+        upper = case_when(upper > 1 ~ 1,
+                          TRUE ~ upper),
+        lower = case_when(upper < 0 ~ 0,
+                          TRUE ~ lower)
+    ) %>%
+    ggplot(aes(x = recall, y = mean_precision,
+             ymin = lower, ymax = upper)) +
+    geom_ribbon(fill = greens[3]) +
+    geom_line(color = greens[9]) +
+    coord_equal() +
+    geom_abline(intercept = 1, slope = 1, linetype="dashed", color="grey50") +
+    scale_y_continuous(expand = c(0,0), limits = c(-0.01,1.01)) +
+    scale_x_reverse(expand = c(0,0), limits = c(1.01,-0.01)) +
+    labs(x = 'Recall', y = 'Precision') +
+    guides(color = guide_legend(nrow = 1,title=""),
+           fill = guide_legend(nrow = 1,title="")) +
+    theme_bw() #+theme(plot.margin = unit(x = c(0, 0, 0, 0), units = "pt"))
 
-  if(x > max(lookup$specificity)){
-    tibble(specificity = x, sensitivity=0)
-  } else {
-    lookup %>%
-      filter(specificity - x > 0) %>%
-      top_n(sensitivity, n=1) %>%
-      summarize(specificity = x,
-                sensitivity = unique(sensitivity))
-  }
-
-}
-
-pool_sens_spec <- function(file_name, specificities){
-
-  load(file_name)
-
-  prob <- predict(model$trained_model, model$test_data, type="prob")
-
-  prob_obs <- bind_cols(prob_srn = prob$cancer,
-                        observed=model$test_data$dx)
-
-  sens_spec_lookup <- get_sens_spec_lookup(prob_obs)
-
-  map_dfr(specificities, get_sensitivity, lookup=sens_spec_lookup) %>%
-    mutate(model = str_replace(file_name,
-                               "data/(.*)_models/rf_model.\\d*.Rdata", "\\1"),
-           seed = str_replace(file_name,
-                              "data/.*_models/rf_model.(\\d*).Rdata", "\\1"),
-    )
-
-}
-
-specificities <- seq(0, 1, 0.01)
-
-
-rf_otu <- list.files(path="data/otu_models",
-                     pattern="rf_model.*.Rdata",
-                     full.names=TRUE) %>%
-  map_dfr(pool_sens_spec, specificities)
-rf_phylum <- list.files(path="data/phylum_models",
-                     pattern="rf_model.*.Rdata",
-                     full.names=TRUE) %>%
-  map_dfr(pool_sens_spec, specificities)
-rf <- bind_rows(rf_otu,rf_phylum)
-write_csv(rf,"data/pat_rf.csv")
-
-rf %>%
-  group_by(model, specificity) %>%
-  summarize(
-    lquartile = quantile(sensitivity, prob=0.25),
-    uquartile = quantile(sensitivity, prob=0.75),
-    sensitivity=median(sensitivity),
-    .groups="drop") %>%
-  ggplot(aes(x=1-specificity, y=sensitivity, fill=model)) +
-  geom_abline(intercept=0, slope=1, color="darkgray") +
-  geom_ribbon(aes(ymin=lquartile, ymax=uquartile), alpha=0.2) +
-  geom_step(aes(color=model)) +
-  theme_classic() +
-  theme(legend.position=c(0.8, 0.2)) +
-  coord_equal()
+cowplot::plot_grid(auroc, auprc) %>%
+    ggsave(snakemake@output[['plot']])
