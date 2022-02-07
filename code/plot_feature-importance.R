eecdf588909785a6aa640360db37aa6266960613
Kelly Sovacool <kellysovacool@gmail.com>
2022-02-07 11:52:46 -0500

    Add genus labels & include OTUs significant in >40% of models

--- code/plot_feature-importance.R
+++ code/plot_feature-importance.R
@@ -1,22 +1,24 @@
 source(here::here('code', 'log_smk.R'))
-library(here)
+library(ggtext)
 library(tidyverse)
 
 alpha_level <- 0.05
-feat_dat <- read_csv(snakemake@input[['csv']]) %>%
-    rename(feature = names)
+feat_dat <- read_csv('results/feature-importance_results.csv') %>%
+    rename(otu = names)
+tax_dat <- schtools::read_tax('data/raw/final.taxonomy.csv') %>%
+    rename(otu = OTU) %>%
+    mutate(label = str_replace(tax_otu_label, '(^\\w+) (.*)', '_\\1_ \\2'))
 
 nseeds <- feat_dat %>% pull(seed) %>% unique() %>% length()
 
 signif_feats <- feat_dat %>%
     filter(pvalue < alpha_level) %>%
-    group_by(feature) %>%
-    summarize(frac_sig = n() / nseeds) %>%
-    filter(frac_sig > 0.5)
+    group_by(otu) %>%
+    summarize(frac_sig = n() / nseeds) %>% filter(frac_sig > 0.4)
 
 feats <- feat_dat %>%
-    filter(feature %in% signif_feats$feature) %>%
-    group_by(feature) %>%
+    filter(otu %in% signif_feats$otu) %>%
+    group_by(otu) %>%
     summarise(mean_auroc = mean(perf_metric),
               sd_auroc = sd(perf_metric),
               mean_diff = mean(perf_metric_diff),
@@ -24,23 +26,24 @@ feats <- feat_dat %>%
               sd_diff = sd(perf_metric_diff),
               lowerq = quantile(perf_metric_diff)[2],
               upperq = quantile(perf_metric_diff)[4]) %>%
-    filter(mean_diff > 0) %>%
-    inner_join(signif_feats, by = 'feature') %>%
+    inner_join(signif_feats, by = 'otu') %>%
+    left_join(tax_dat %>% select(otu, label), by = 'otu') %>%
     arrange(mean_diff)
 
 top_feats <- feats %>%
     slice_max(n = 20, order_by = mean_diff) %>%
-    mutate(feature = fct_reorder(as.factor(feature), mean_diff))
+    mutate(label = fct_reorder(as.factor(label), mean_diff),
+           percent_models_signif = frac_sig * 100)
 
 feat_imp_plot <- top_feats %>%
-    mutate(percent_models_signif = frac_sig * 100) %>%
-    ggplot(aes(x = mean_diff, y = feature, color = percent_models_signif)) +
+    ggplot(aes(x = -mean_diff, y = label, color = percent_models_signif)) +
     geom_vline(xintercept = 0, linetype = 'dashed') +
-    geom_pointrange(aes(xmin = mean_diff - sd_diff, xmax = mean_diff + sd_diff)) +
+    geom_pointrange(aes(xmin = -mean_diff - sd_diff, xmax = -mean_diff + sd_diff)) +
     scale_color_continuous(type = 'viridis', name = '% models') +
-    labs(y = '', x = 'Mean difference in AUROC') +
+    labs(y = '', x = 'Mean decrease in AUROC') +
     theme_bw() +
-    theme(legend.position = 'bottom',
+    theme(axis.text.y = element_markdown(),
+          legend.position = 'bottom',
           legend.margin = margin(t = 0, r = 0, b = 0, l = 0, unit = "pt"),
           plot.margin = unit(x = c(0, 0, 0, 0), units = "pt"))
 
