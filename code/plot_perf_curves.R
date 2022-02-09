f760a10c77c419b95fc57f0afee5fd18c2bcc3ce
Kelly Sovacool <kellysovacool@gmail.com>
2022-02-09 15:52:55 -0500

    Annotate baseline precision

--- code/plot_perf_curves.R
+++ code/plot_perf_curves.R
@@ -1,6 +1,8 @@
 source("code/plotting-functions.R")
 
 sens_dat <- read_csv(snakemake@input[["csv"]])
+metadat <- readxl::read_excel("data/raw/ml_metadata.xlsx") %>%
+  rename(sample = group)
 
 auroc_step <- sens_dat %>%
   filter(seed == 100) %>%
@@ -15,8 +17,15 @@ auroc_step <- sens_dat %>%
   # scale_color_grey() +
   theme(legend.position = "none")
 
-auc_plots <- plot_roc(calc_roc(sens_dat)) + plot_prc(calc_prc(sens_dat))
+cdiff_tally <- metadat %>% group_by(pos_cdiff_d1) %>% tally()
+npos <- cdiff_tally %>% filter(pos_cdiff_d1 == 'yes') %>% pull(n)
+ntot <- cdiff_tally %>% pull(n) %>% sum()
+baseline_prec <- npos / ntot
+prc_plot <- sens_dat %>%
+  calc_prc() %>%
+  plot_prc(baseline_prec = baseline_prec)
 
+auc_plots <- plot_roc(calc_roc(sens_dat)) + prc_plot
 ggsave(
   plot = auc_plots, filename = snakemake@output[["plot"]],
   dpi = 200, units = "in", width = 6, height = 5
